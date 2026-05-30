e8c4019a2bb2ac0fed7afce26b7388a0a7f75ee3
angiepotts <angeledwar821d@gmail.com>
2026-05-30 09:27:28 +0300

    Updated backend API

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ app.post('/contact', (req, res) => {
 
   const { name, email, message } = req.body
 
-  console.log(name, email, message)
+  console.log({
+    name,
+    email,
+    message
+  })
 
   res.json({
     success: true,
